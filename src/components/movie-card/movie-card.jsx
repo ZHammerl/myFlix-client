8b457d25a3c59e27587e7a0ec7b1e70ec8c0da33
ZHammerl <108287700+ZHammerl@users.noreply.github.com>
2022-08-29 16:19:05 +0200

    adding movie-card component

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export class MovieCard extends React.Component {
+  render() {
+    const { movieData, onMovieClick } = this.props;
+    return <div className="movie-card" onClick={()=>{onMovieClick(movieData);}}>{movieData.Title}</div>;
+  }
+}
