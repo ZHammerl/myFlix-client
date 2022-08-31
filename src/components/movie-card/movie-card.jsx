8343e145991e9a85c71e8a977753a778c4312b14
ZHammerl <108287700+ZHammerl@users.noreply.github.com>
2022-08-31 19:22:52 +0200

    Cards display

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
   render() {
     const { movieData, onMovieClick } = this.props;
     return (
-      <div
-        className="movie-card"
-        onClick={() => {
-          onMovieClick(movieData);
-        }}>
-        {movieData.Title}
-      </div>
+      <Card>
+        <Card.Img variant="top" src={movieData.Imageurl} />
+        <Card.Body>
+          <Card.Title> {movieData.Title}</Card.Title>
+          <Card.Text>{movieData.Description}</Card.Text>
+          <Button
+            onClick={() => {
+              onMovieClick(movieData);
+            }}
+            variant="link">
+            Show Details
+          </Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
